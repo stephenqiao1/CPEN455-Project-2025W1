d252bf38067e1873741e533e8279131c29399907
Stephen Qiao <sqiao01@dhcp-206-87-131-119.ubcsecure.wireless.ubc.ca>
2025-11-26 18:06:54 -0800

    updated files

--- examples/bayes_inverse_full_finetune.sh
+++ examples/bayes_inverse_full_finetune.sh
@@ -3,7 +3,6 @@ uv run -m examples.bayes_inverse \
 --method full_finetune \
 --max_seq_len 256 \
 --batch_size 8 \
---num_iterations 300 \
+--num_iterations 150 \
 --learning_rate 1e-5 \
---patience 50 \
 --dataset_path autograder/cpen455_released_datasets/augmented_train_val_subset.csv
